a1570fee81e4b944f4cdc74350a1284533716d1f
rcastillod <rodrigo.castillo@global3.cl>
2023-02-27 16:54:47 -0300

    feat: :sparkles: Custom cursor

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,45 @@ gsap.registerPlugin(ScrollTrigger);
 import { Flip } from 'gsap/Flip';
 gsap.registerPlugin(Flip);
 
+// Custom cursor
+let cursor = document.querySelector('.cursor'),
+  cursorScale = document.querySelectorAll('.cursor-scale'),
+  mouseX = 0,
+  mouseY = 0
+
+gsap.to({}, 0.016, {
+  repeat: -1,
+
+  onRepeat: function () {
+    gsap.set(cursor, {
+      css: {
+        left: mouseX,
+        top: mouseY
+      }
+    })
+  }
+});
+
+window.addEventListener("mousemove", function (e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY
+});
+
+cursorScale.forEach(link => {
+  link.addEventListener('mouseleave', () => {
+    cursor.classList.remove('grow');
+    cursor.classList.remove('grow-small');
+  });
+  link.addEventListener('mousemove', () => {
+    cursor.classList.add('grow');
+    if (link.classList.contains('small')) {
+      cursor.classList.remove('grow');
+      cursor.classList.add('grow-small');
+    }
+  });
+});
+
+
 // Lenis smooth scrolling
 let lenis;
 // Initialize Lenis smooth scrolling
