443971166aa7db50c09ad430bb91696c6715ec67
rcastillod <rodrigo.castillo@global3.cl>
2024-01-03 17:29:03 -0300

    feat: Add github to social list

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
-  darkMode: 'class',
-  theme: {
-    fontFamily: {
-      sans: ['Syne', 'sans-serif'],
-      'poppins': ['Poppins', 'sans-serif'],
-    },
-    extend: {
-      colors: {
-        'primary': '#161519',
-        'primary-shade': '#131216',
-        'primary-tint': '#2d2c30',
-        'primary-light': '#FAF9FE',
-        'secondary': '#49da94',
-        'secondary-shade': '#40c082',
-        'secondary-tint': '#5bde9f',
-        'grey': '#69686D',
-        'linkedin': '#0077b5',
-        'behance': '#053eff ',
-      },
-    },
-  },
-  plugins: [],
+	content: ["./*.{html,js}"],
+	darkMode: 'class',
+	theme: {
+		fontFamily: {
+			sans: ['Syne', 'sans-serif'],
+			'poppins': ['Poppins', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'primary': '#161519',
+				'primary-shade': '#131216',
+				'primary-tint': '#2d2c30',
+				'primary-light': '#FAF9FE',
+				'secondary': '#49da94',
+				'secondary-shade': '#40c082',
+				'secondary-tint': '#5bde9f',
+				'grey': '#69686D',
+				'linkedin': '#0077b5',
+				'github': '#24292E ',
+				'behance': '#053eff ',
+			},
+		},
+	},
+	plugins: [],
 }
